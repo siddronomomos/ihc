05ba3a826ea29c041cd7b2a786fb212d38935929
siddronomomos <christopheralcocer938@gmail.com>
2025-04-07 20:54:52 -0600

    listview

--- app/src/main/java/com/example/ihc/MaterialListActivity.java
+++ app/src/main/java/com/example/ihc/MaterialListActivity.java
@@ -2,18 +2,23 @@ package com.example.ihc;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MaterialListActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_list);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
 
         String[] materials = {
                 "Papel y cartón",
@@ -34,14 +39,40 @@ public class MaterialListActivity extends AppCompatActivity {
         );
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedMaterial = materials[position];
-                Toast.makeText(MaterialListActivity.this,
-                        "Diste clic a " + selectedMaterial,
-                        Toast.LENGTH_SHORT).show();
-            }
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedMaterial = materials[position];
+            showRecyclingInfo(selectedMaterial);
         });
+    }
+
+    private void showRecyclingInfo(String material) {
+        String url = getRecyclingUrl(material);
+        webView.loadUrl(url);
+        webView.setVisibility(View.VISIBLE);
+        findViewById(R.id.materialListView).setVisibility(View.GONE);
+    }
+
+    private String getRecyclingUrl(String material) {
+        switch (material) {
+            case "Papel y cartón":
+                return "https://es.wikipedia.org/wiki/Reciclaje_de_papel";
+            case "Vidrio":
+                return "https://es.wikipedia.org/wiki/Reciclaje_de_vidrio";
+            case "Plástico":
+                return "https://es.wikipedia.org/wiki/Reciclaje_de_pl%C3%A1stico";
+            // Agrega más casos según sea necesario
+            default:
+                return "https://es.wikipedia.org/wiki/Reciclaje";
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.getVisibility() == View.VISIBLE) {
+            webView.setVisibility(View.GONE);
+            findViewById(R.id.materialListView).setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
